bdcf4d5944388f90a5dee62401b3efc2675a35ee
ha1yuan <ha1yuan_dev@outlook.com>
2025-09-14 21:57:28 +0800

    feat: import LongBridge

---
+++ src/main/java/io/github/ha1yuan/common/Logs.java
@@ -0,0 +1,14 @@
+package io.github.ha1yuan.common;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Logs {
+
+    public static final Logger bizLog = LoggerFactory.getLogger("biz");
+
+    public static final Logger expLog = LoggerFactory.getLogger("exception");
+
+    public static final Logger buryLog = LoggerFactory.getLogger("bury");
+
+}
--- src/main/java/io/github/ha1yuan/infra/LongBridge.java
+++ src/main/java/io/github/ha1yuan/infra/LongBridge.java
@@ -1,15 +1,52 @@
 package io.github.ha1yuan.infra;
 
 import com.longport.Config;
+import com.longport.ConfigBuilder;
+import com.longport.trade.AccountBalance;
+import com.longport.trade.TradeContext;
+import io.github.ha1yuan.common.Logs;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 
 @Component
 public class LongBridge {
 
+    @Value("${longport.app.key}")
+    private String appKey;
+    @Value("${longport.app.secret}")
+    private String appSecret;
+    @Value("${longport.access.token}")
+    private String appAccessToken;
 
-    Config config = Config.fromEnv();
+    private Config config;
 
+    @SneakyThrows
+    @PostConstruct
+    public void init() {
 
+        config = new ConfigBuilder(appKey, appSecret, appAccessToken).build();
+        Logs.bizLog.info("LongBridge init success");
 
+    }
 
+    public void getContext() {
+
+        try {
+
+            TradeContext ctx = TradeContext.create(config).get();
+            Logs.bizLog.info("LongBridge getContext success");
+
+            for (AccountBalance accountBalance : ctx.getAccountBalance().get()) {
+                Logs.bizLog.info(accountBalance.toString());
+            }
+
+        } catch (Exception e) {
+
+            Logs.bizLog.error("LongBridge getContext error", e);
+
+        }
+    }
 }
