f9c024caa32914339c338d12f49eae07902f672c
ha1yuan <ha1yuan_dev@outlook.com>
2025-09-14 21:07:58 +0800

    init modules

---
+++ src/main/java/io/github/ha1yuan/infra/LongBridge.java
@@ -0,0 +1,15 @@
+package io.github.ha1yuan.infra;
+
+import com.longport.Config;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LongBridge {
+
+
+    Config config = Config.fromEnv();
+
+
+
+
+}
