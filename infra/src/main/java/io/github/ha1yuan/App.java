b1c6dbc67c65586265657f143f9a71df6bab8643
ha1yuan <cn.lk@qq.com>
2025-09-14 16:44:11 +0800

    add modules

---
+++ infra/src/main/java/io/github/ha1yuan/App.java
@@ -0,0 +1,13 @@
+package io.github.ha1yuan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
